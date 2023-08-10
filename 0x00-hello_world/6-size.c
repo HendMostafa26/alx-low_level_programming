2851af5da4462068338a26ff61dd6850c820cfc5
HendMostafa26 <hendmustafa26@gmail.com>
2023-08-10 08:32:11 -0700

    Size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ *main - Entry point to function printf
+ *return: 0 is success
+ */
+int main(void)
+{
+char c;
+int i;
+float f;
+printf("Size of a char: %lu.\n" (unsigned char)size of(c));
+printf("Size of an int: %lu.\n" (unsigned int)size of (i));
+printf("Size of a long int: %ln.\n" (unsigned long int) size of (i));
+printf("Size of a long long int: 5ln.\n" (unsigned long long int) size of (i));
+printf("Size of a float: %ln.\n" (unsigned float) size of (f));
+return (0);
+}
+  
