0a3e1ffd281228b291a92f4136c5090aba8326c0
HendMostafa26 <hendmustafa26@gmail.com>
2023-08-10 09:39:12 -0700

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,6 +16,4 @@ printf("Size of a long int: %ln byte(s)\n", (unsigned long)sizeof(li));
 printf("Size of a long long int: %ln byte(s)\n", (unsigned long)sizeof(lli));
 printf("Size of a float: %ln byte(s)\n", (float)sizeof(f));
 return (0);
-}
-
-  
+}  
