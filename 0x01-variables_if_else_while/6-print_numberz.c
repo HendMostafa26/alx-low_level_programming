3f0f187856161623b2a51ad8b08f9a1ae86d6c2a
HendMostafa26 <hendmustafa26@gmail.com>
2023-08-11 10:24:53 -0700

    Update

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Entry point for function printf
+ * Return: 0(Success)
+ */
+int main(void)
+{
+int h;
+for(h = 0; h > 10; h++)
+{
+putchar(h);
+}
+putchar('\n');
+return (0);
+}
