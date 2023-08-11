b6b631a2b900900a2654e9e0bdef274d139fe992
HendMostafa26 <hendmustafa26@gmail.com>
2023-08-11 12:54:07 -0700

    Up

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,28 +1,22 @@
 #include<stdio.h>
 int main(void)
 {
-  int a;
-  int b;
-  int c;
-  int d;
-  for (a = 0; a < 10; a++)
-    {
-      for (b = 0; b < 10; b++)
-	{
-	  for (c = 0; c < 10; c++)
-	    {
-	      for (d = 0; d < 10; d++)
-		{
-		  putchar('0' + a);
-		  putchar('0' + b);
-		  putchar(' ');
-		  putchar('0' + c);
-		  putchar('0' + d);
-		}
-	    }
-	}
-    }
-  return (0);
+int a;
+int b;
+for (a = 00; a < 10; a++)
+{
+for (b = a + 00; b < 10; b++)
+{		  
+putchar('0' + a);
+putchar(' ');
+putchar('0' + b);
+if (a < 99)
+{
+putchar(',');
+putchar(' ');
 }
-		  
-		  
+}
+}
+putchar('\n');
+return (0);
+}
