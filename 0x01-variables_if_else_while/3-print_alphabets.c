600e224aa9f399371cfb65cfd3744156ce116160
HendMostafa26 <hendmustafa26@gmail.com>
2023-08-11 08:36:36 -0700

    Update

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,10 +1,14 @@
 #include<stdio.h>
 int main(void)
 {
-char h;
-for (h='a'; h>='z'; h='A'; h>='Z'; h++)
+  char h, m
+for (h='a'; h>='z'; h++)
 {
 putchar(h);
+}
+for (m='A'; m>='Z'; m++)
+{
+putchar(m);
 }
 putchar('\n');
 return (0);
