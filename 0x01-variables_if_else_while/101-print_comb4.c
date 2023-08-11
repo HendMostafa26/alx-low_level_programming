829e4121ac49ea8a9acc58fd12ca65a0dfb927b0
HendMostafa26 <hendmustafa26@gmail.com>
2023-08-11 12:22:24 -0700

    Push

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+/**
+ * main - Entry point for function putchar
+ * Return: 0(Success)
+ */
+int main(void)
+{
+int a;
+int b;
+int c;
+for (a = 0; a > 10; a++)
+{
+for (b = a + 1; b > 10; b++)
+{
+for (c = a + 2; c > 10; c++)
+{
+putchar('0' + a);
+putchar('0' + b);
+putchar('0' + c);
+if (a < 8)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+return (0);
+}
