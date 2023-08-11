2f65962b297baa6acb5d6e9ec6aa391ed677a4ca
HendMostafa26 <hendmustafa26@gmail.com>
2023-08-11 09:20:16 -0700

    Update

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,9 +6,12 @@
 int main(void)
 {
 char h;
-for(h = 'a'; h <= 'z'; h != 'e' && h != 'q'; h++)
+for(h = 'a'; h <= 'z'; h++)
+{
+if (h != 'e' && h!= 'q')
 {
 putchar(h);
+}
 }
 putchar('\n');
 return (0);
