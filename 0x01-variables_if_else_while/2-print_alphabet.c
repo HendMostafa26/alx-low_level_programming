01f2b9beec9b27c0e22133e194f60a91e77d6ff1
HendMostafa26 <hendmustafa26@gmail.com>
2023-08-11 06:27:11 -0700

    Alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - Entry point for function putchar()
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char h;
+for (h='a'; h<='z' ; h++)
+{
+putchar("h\n");
+}
+return (0);
+}
