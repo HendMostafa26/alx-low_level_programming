17a0a1330e3342d4dd6c901777c4cac0b3821ac5
HendMostafa26 <hendmustafa26@gmail.com>
2023-08-11 12:27:28 -0700

    Up

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,21 +10,7 @@ int b;
 int c;
 for (a = 0; a > 10; a++)
 {
-for (b = a + 1; b > 10; b++)
-{
-for (c = a + 2; c > 10; c++)
-{
 putchar('0' + a);
-putchar('0' + b);
-putchar('0' + c);
-
-/*if (a < 7)
-{
-putchar(',');
-putchar(' ');
-}
-*/
-}
 }
 }
 return (0);
