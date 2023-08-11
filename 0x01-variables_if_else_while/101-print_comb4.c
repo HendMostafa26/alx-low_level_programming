1862f041bed17c6c413628ac813cfc37c6f14b77
HendMostafa26 <hendmustafa26@gmail.com>
2023-08-11 12:36:38 -0700

    Update

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
-/**                                                                                                                                                                      
- * main - Entry point for function putchar                                                                                                                               
- * Return: 0(Success)                                                                                                                                                    
+/**                                                                                                                                                                     
+ * main - Entry point for function putchar                                                                                                                              
+ * Return: 0(Success)                                                                                                                                                   
  */
 int main(void)
 {
