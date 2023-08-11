04da3514acde9079e28792d29535e7289953395e
HendMostafa26 <hendmustafa26@gmail.com>
2023-08-11 11:13:34 -0700

    Update

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - Entry point for function putchar
+ * Return: 0(Success)
+ */
+int main(void)
+{
+int h;
+for (h = 0; h < 10; h++)
+{
+putchar('0' + h);
+putchar(',');
+}
+putchar('\n');
+return(0);
+}
