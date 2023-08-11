24a9f39efdcb87c35c716c95357d3c3016993d00
HendMostafa26 <hendmustafa26@gmail.com>
2023-08-11 12:12:11 -0700

    Push

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,13 +8,13 @@ int main(void)
 int a;
 int b;
 for (a = 0; a < 10; a++)
-  {
+{
 for (b = a + 1; b < 10; b++)
- {
-if (a < 9)
 {
 putchar('0' + a);
 putchar('0' + b);
+if (a <9)
+{
 putchar(',');
 putchar(' ');
 }
