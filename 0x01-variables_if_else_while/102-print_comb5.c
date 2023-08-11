f3e764543b6d72e1235f0dd8a8c4916f081c0254
HendMostafa26 <hendmustafa26@gmail.com>
2023-08-11 13:17:45 -0700

    Up

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,11 +13,11 @@ for (b = 0; b <= 99; b++)
 {		  
 if (a < b && a!= b)
 {
-putchar('0' + a / 10);
-putchar('0' + a % 10);
+putchar('0' + (a / 10));
+putchar('0' + (a % 10));
 putchar(' ');
-putchar('0' + b / 10);
-putchar('0' + b % 10);
+putchar('0' + (b / 10));
+putchar('0' + (b % 10));
 if (a != 98 && b != 99)
 {
 putchar(',');
