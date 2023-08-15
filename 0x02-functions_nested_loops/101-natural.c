d4d46717d77ede49aecbcecd1e3a4966225f4f7c
HendMostafa26 <hendmustafa26@gmail.com>
2023-08-15 12:54:37 -0700

    Push

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,9 +8,9 @@ int main(void)
 int a, b = 0;
 for (a = 0; a < 1024; a++)
 {
-if ((a % 3 == 0) || (a % 5 ==0))
+if ((a % 3 == 0) || (a % 5 == 0))
 b = b + a;
 }
 printf("%d\n", b);
-return(0);
+return (0);
 }
