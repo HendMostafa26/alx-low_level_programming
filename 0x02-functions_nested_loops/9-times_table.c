da938606aaa8257110da95ba041defabe6fd3fc9
HendMostafa26 <hendmustafa26@gmail.com>
2023-08-15 08:21:59 -0700

    Push

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+ * times_table - Function to print all time tables
+ * Return: 0 and 1
+ */
+void times_table(void)
+{
+int i, n;
+for (i = 0; i <= 10; i++)
+times = n * i;
+_putchar('0' + times);
+return (times);
+}
+  
