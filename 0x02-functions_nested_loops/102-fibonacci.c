db06469db7c280cae2543407f522c9033dbd4980
HendMostafa26 <hendmustafa26@gmail.com>
2023-08-15 13:03:24 -0700

    Push

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+/**
+ * main - Entry point for function printf
+ * Return: 0 or 1
+ */
+int main(void)
+{
+int a = 0, b = 1, c, sum;
+for (c = 0; c < 50; c++)
+{
+sum = a + b;
+printf("%d\n", sum);
+a = b;
+b = sum;
+if (c == 49)
+{
+printf("\n");
+}
+else
+{
+print(", ");
+}
+}
+return (0);
+}
