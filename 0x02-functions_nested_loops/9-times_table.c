2b505aefb527539252dec1916e2659a1013d0ef1
HendMostafa26 <hendmustafa26@gmail.com>
2023-08-15 09:24:15 -0700

    Push

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,24 +5,32 @@
  */
 void times_table(void)
 {
-  int a, b, c, d, e;
-  for (a = 0; a <= 9; a++)
-    {
-      for (b = 0; b <= 9; b++)
-	{
-	  c = a * b;
-	  if (c > 9)
-	    {
-	      d = c % 10;
-	      e = (d - c) / 10;
-	  _putchar(',');
-	  _putchar(' ');
-	  _putchar('0' + d);
-	  _putchar('0' + e);
-      _putchar('0' + c);
-    }
-    }
-    }
-  _putchar('\n');
+int a, b, c, d, e;
+for (a = 0; a <= 9; a++)
+{
+for (b = 0; b <= 9; b++)
+{
+c = a * b;
+if (c > 9)
+{
+d = c % 10;
+e = (d - c) / 10;
+_putchar(',');
+_putchar(' ');
+_putchar('0' + d);
+_putchar('0' + e);
+}
+else
+{
+if (b != 0)
+{
+_putchar(',');
+_putchar(' ');
+}
+_putchar('0' + c);
+}
+}
+_putchar('\n');
+}
 }
   
