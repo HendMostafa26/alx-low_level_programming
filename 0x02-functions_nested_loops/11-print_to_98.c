0ea4af0a737d00f13f6da2cd2c37fbf1a66c571d
HendMostafa26 <hendmustafa26@gmail.com>
2023-08-15 11:15:52 -0700

    Push

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * print_to_98 - function that prints numbers from n to 98
+ * @n: Integer number
+ * Return: void
+ */
+void print_to_98(int n)
+{
+for (; n <= 98; n++)
+{
+if (n == 98)
+{
+_putchar('0' + n);
+_putchar(',');
+_putchar(' ');
+_putchar('\n');
+}
+else
+{
+_putchar(' ');
+_putchar(',');
+_putchar(' ');
+}
+}
+for (; n >= 98; n--)
+{
+if (n ==98)
+{
+_putchar('0' + n);
+_putchar(',');
+_putchar(' ');
+_putchar('\n');
+}
+else
+{
+_putchar(' ');
+_putchar(',');
+_putchar(' ');
+}
+}
+}
