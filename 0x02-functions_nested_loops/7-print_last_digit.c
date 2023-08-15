a7db70c86a64c46f0a11f31abbca8659da831e36
HendMostafa26 <hendmustafa26@gmail.com>
2023-08-15 06:01:19 -0700

    Push

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,9 +1,14 @@
 #include"main.h"
+/**
+ * print_last_digit - Prints last digit
+ * @h: Character
+ * Return: 1 and 0
+ */
 int print_last_digit(int h)
 {
 int last_digit;
 last_digit = h % 10;
-if (last < 0)
+if (last_digit < 0)
 {
 last_digit = last_digit * 10;
 }
