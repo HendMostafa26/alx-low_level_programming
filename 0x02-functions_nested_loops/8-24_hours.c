c0f38436c43fdc1b9fb18b9c4f65191a0e380213
HendMostafa26 <hendmustafa26@gmail.com>
2023-08-15 08:07:56 -0700

    Push

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,12 +5,12 @@
  */
 void jack_bauer(void)
 {
-int a,b,c,d;
+int a, b, c, d;
 for (a = 0; a <= 2; a++)
 {
 for (b = 0; b <= 9; b++)
 {
-if (( a <= 1 && b <= 9) || (a <= 2 && b <= 3))
+if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
 {
 for (c = 0; c <= 5; c++)
 {
@@ -19,7 +19,7 @@ for (d = 0; d <= 9; d++)
 _putchar('0' + a);
 _putchar('0' + b);
 _putchar(':');
-_putchar('0'+ c);
+_putchar('0' + c);
 _putchar('0' + d);
 _putchar('\n');
 }
