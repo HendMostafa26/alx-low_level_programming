8fc8a44dbe3fc92fde2bec05d80b08f62b64fa2d
HendMostafa26 <hendmustafa26@gmail.com>
2023-08-15 12:12:29 -0700

    Push

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,36 +1,29 @@
 #include"main.h"
+/**
+ * print_times_table: Print time table
+ * @n: Integer
+ * Return: void
+ */
 void print_times_table(int n)
 {
-  int a, b, c, d, e;
-  if(n <= 15 && n >= 0)
-    {
-      for (a = 0; a <= 15; a++)
-	{
-	  for (b = 0; b <= 15; b++)
-	    {
-	      c = a * b;
-	      if (c > 15)
-		{
-		  d = c % 10;
-		  e = (c - d) / 10;
-		  _putchar(',');
-		  _putchar(' ');
-		  _putchar('0' + e);
-		  _putchar('0' + d);
-		}
-	      else
-		{
-		  if (b != 0)
-		    {
-		      _putchar(',');
-		      _putchar(' ');
-		      _putchar(' ');
-		    }
-		  _putchar('0' + c);
-		}
-	    }
-	  _putchar('\n');
-	}
-    }
+int a, b, c;
+if(n <= 15 && n >= 0)
+{
+for (a = 0; a <= n; a++)
+{
+for (b = 0; b <= n; b++)
+c = a * b
+if (b == 0)
+{
+_putchar('0' + c);
+else if (c < 10 && b != 0)
+{
+_putchar(',');
+_putchar(' ')
 }
-	  
+}
+_putchar('\n');
+}
+}
+}
+		
