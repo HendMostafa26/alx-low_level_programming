63c05edd32d70e08253718bff29cddd57a182722
HendMostafa26 <hendmustafa26@gmail.com>
2023-08-15 12:01:57 -0700

    Push

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,36 @@
+#include"main.h"
+void print_times_table(int n);
+{
+  int a, b, c, d, e;
+  if(n <= 15 && n >= 0)
+    {
+      for (a = 0; a <= 15; a++)
+	{
+	  for (b = 0; b <= 15; b++)
+	    {
+	      c = a * b;
+	      if (c > 15)
+		{
+		  d = c % 10;
+		  e = (c - d) / 10;
+		  _putchar(',');
+		  _putchar(' ');
+		  _putchar('0' + e);
+		  _putchar('0' + d);
+		}
+	      else
+		{
+		  if (b != 0)
+		    {
+		      _putchar(',');
+		      _putchar(' ');
+		      _putchar(' ');
+		    }
+		  _putchar('0' + c);
+		}
+	    }
+	  _putchar('\n');
+	}
+    }
+}
+	  
