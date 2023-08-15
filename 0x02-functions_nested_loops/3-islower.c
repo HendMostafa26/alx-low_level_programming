608a1bea2805f7643fe67535572e82179689d19c
HendMostafa26 <hendmustafa26@gmail.com>
2023-08-15 04:21:58 -0700

    Push

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,16 +2,15 @@
 /**
  * _islower - Function used to see if it is lower or not
  * Return: 0 is success and 1 is failure
+ * c: Letter
  */
 int _islower(int c)
 {
-if (c == 'c')
-{
-return (0);
-}
-else
+if (c >= 97 && c <= 122)
 {
 return (1);
 }
+else
+{
 return (0);
 }
