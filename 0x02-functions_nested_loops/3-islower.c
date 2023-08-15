baa0361c046c0c3f1c46df59670f86794350b279
HendMostafa26 <hendmustafa26@gmail.com>
2023-08-15 04:10:12 -0700

    Push

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+ * _islower - Function used to see if it is lower or not
+ * Return: 0 is success and 1 is failure
+ */
+int _islower(int c)
+{
+int h;
+if (h == 'c')
+{
+return (0);
+}
+else
+{
+return(1);
+}
+return (0);
+}
