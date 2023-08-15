ef7fba32cfdd3cf36efd0146c5611a3b7c5e425c
HendMostafa26 <hendmustafa26@gmail.com>
2023-08-15 03:45:56 -0700

    Push

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,12 +7,12 @@ void print_alphabet_x10(void)
 {
 int i;
 char h;
-for (h = 'a'; h <= 'z'; h++)
-{
 for (i = 0; i < 10; i++)
+{
+for (h = 'a'; h <= 'z'; h++)
 {
 _putchar(h);
 }
-}
 _putchar('\n');
+}
 }
