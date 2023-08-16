6ab7368c66040ddda9b497238b8f8d2f2b7157a9
HendMostafa26 <hendmustafa26@gmail.com>
2023-08-16 00:01:42 -0700

    Main Files

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,7 +1,4 @@
-#include <stdio.h>
 #include"main.h"
-#include <stdlib.h>
-#include <time.h>
 /**
  * positive_or_negative - Function to test if the number is positive or negative
  * @i: Integer
@@ -9,8 +6,6 @@
  */
 void positive_or_negative(int i)
 {
-srand(time(0));
-i = rand() - RAND_MAX / 2;
 if (i > 0)
 printf("%d is positive\n", i);
 else if (i < 0)
