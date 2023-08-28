6c70e3b02384ecb70b90a62fcf33aaba99bc02d3
HendMostafa26 <hendmustafa26@gmail.com>
2023-08-28 01:38:55 -0700

    strchr

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,5 +5,6 @@ int add(int a, int b);
 char *_memset(char *s, char b, unsigned int n);
 int _putchar(char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 
 #endif
