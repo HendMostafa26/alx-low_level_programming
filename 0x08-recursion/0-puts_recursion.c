e5fbed37959ecb598501546b571f5099955fdfa5
HendMostafa26 <hendmustafa26@gmail.com>
2023-08-30 01:49:17 -0700

    puts recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,10 +6,13 @@
  */
 void _puts_recursion(char *s)
 {
-int i;
-for (i = 0; s[i]; i++)
+if (*s != '\0')
 {
-_putchar (s[i]);
+  _putchar(*s);
+  _puts_recursion(s + 1);
 }
-_putchar('\n');
+ else
+   {
+     _putchar('\n');
+   }
 }
