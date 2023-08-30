03119f21ab573cc12981c5add1f131c59d352b28
HendMostafa26 <hendmustafa26@gmail.com>
2023-08-30 02:14:46 -0700

    strlen recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,11 +6,11 @@
  */
 int _strlen_recursion(char *s)
 {
-int i;
-int j = 0;
-for (i = 0; s[i] != '\0'; i++)
+int i = 0;
+if (*s != '\0')
 {
-j++;
+i++;
+i = i + _strlen_recursion(s + 1);
 }
 return (i);
 }
