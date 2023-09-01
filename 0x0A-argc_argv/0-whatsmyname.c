89c843c8336aa394d2602c142c69e0568049d5df
HendMostafa26 <hendmustafa26@gmail.com>
2023-09-01 08:06:59 -0700

    My Name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * main - Entry point
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: 0 or 1
+ */
+int main (int argc, char *argv[])
+{
+(void) argc;
+printf("%s\n", *argv);
+return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,7 @@
+#ifndef MYHEADERS_H
+#define MYHEADERS_H
+
+int _putchar(char c);
+int _atoi(char *s);
+
+#endif
