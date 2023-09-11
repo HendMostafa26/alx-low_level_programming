935d95ccb4d58a32eceb66b7bbe9036c39052386
HendMostafa26 <hendmustafa26@gmail.com>
2023-09-11 03:43:52 -0700

    Half

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure
+ * @name: string
+ * @age: float
+ * @owner: string
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+
+#endif
